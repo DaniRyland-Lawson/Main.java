0fea458cde134095a59af39ed002dd17862e6b84
Dani Ryland-Lawson <Thismygithub87>
2021-10-24 20:54:16 +1000

    Tidied up code Got RainfallVisualiser to show canvas

--- src/cp2406/RainfallAnalyser.java
+++ src/cp2406/RainfallAnalyser.java
@@ -1,92 +1,94 @@
 package cp2406;
 
+/*
+ CP2406 Programming III Assignment Alpha-Version
+ Danielle Lawson
+ */
+
+
 import com.opencsv.CSVReader;
 import com.opencsv.CSVWriter;
-
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.Arrays;
+import java.io.*;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 
 public class RainfallAnalyser {
-//[Product code,
-// Bureau of Meteorology station number,
-// Year,
-// Month,
-// Day,
-// Rainfall amount (millimetres),
-// Period over which rainfall was measured (days),
-// Quality]
-    public static final int YEAR_IDX = 2;
+    /**
+     Product code [0]
+     Bureau of Meteorology station number [1]
+     Year [2]
+     Month [3]
+     Day [4]
+     Rainfall amount (millimetres) [5]
+     Period over which rainfall was measured (days) [6]
+     Quality [7]
+     */
+
     public static final int MONTH_IDX = 3;
     public static final int DAY_IDX = 4;
     public static final int RAIN_IDX = 5;
 
+
     public static void main(String[] args) {
-        System.out.println("Hello Assign 2 Let's do this");
+//        System.out.println("Hello Assign 2");
 
 // create map
-        HashMap<String, Double> yearMap = new HashMap<>();
         HashMap<String, Double> monthMap = new HashMap<>();
         HashMap<String, Double> dayMin = new HashMap<>();
         HashMap<String, Double> dayMax = new HashMap<>();
+
 // open file
         try {
             FileReader reader = new FileReader(
                     "BOMRainFallFiles/MountSheridanStationCNS.csv");
-//            CSVReaderHeaderAware csv = new CSVReaderHeaderAware(reader);
 
 // read file
             CSVReader csv = new CSVReader(reader);
             List<String[]> arr = csv.readAll();
-            // Removed the bottom
-           // String[] header = arr.remove(0);
-            System.out.println(Arrays.toString(arr.get(0)));
+//            System.out.println(Arrays.toString(arr.get(0)));
             for (String[] row : arr) {
-                System.out.println(Arrays.toString(row));
-                // still to fix is the average per month for the years
-//                collectYear(yearMap, row);
+//                System.out.println(Arrays.toString(row));
                 collectMonth(monthMap, row);
                 collectDay(dayMin, row, true);
                 collectDay(dayMax, row, false);
-//                collectMaxDay(dayMax, row);
             }
-            System.out.println("OK");
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
+//            System.out.println("OK");
+
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        System.out.println(yearMap);
-        System.out.println(monthMap);
-        System.out.println(dayMin);
-        System.out.println(dayMax);
+// testing output
+//        System.out.println(yearMap);
+//        System.out.println(monthMap);
+//        System.out.println(dayMin);
+//        System.out.println(dayMax);
+
+        //create output file called MountSheridanStation_Analysed.csv
+        String outPath = "BOMRainFallFiles/MountSheridanStation_Analysed.csv";
+        String[] header ={"Month", "Total rain", "Month-day", "Min rain", "Max rain"};
+        myWriteToCSV(monthMap, dayMin, dayMax, outPath, header );
 
 
-        String outPath = "BOMRainFallFiles/MyMonthMap.csv";
-        String[] header ={"Month", "Total rain"};
-        myWriteToCSV(monthMap, outPath, header );
 
+/*   Testing of data values into csv files
+        outPath = "BOMRainFallFiles/MyMonthMap.csv";
+        header ={"Month", "Total rain"};
+        myWriteToCSV(monthMap, dayMin, monthMap, outPath, header );
 
         outPath = "BOMRainFallFiles/MyMinDay.csv";
         header = new String[]{"Month-day", "Min rain"};
-        myWriteToCSV(dayMin, outPath, header );
+        myWriteToCSV(monthMap, dayMin, dayMin, outPath, header );
 
         outPath = "BOMRainFallFiles/MyMaxDay.csv";
         header = new String[]{"Month-day", "Max rain"};
-        myWriteToCSV(dayMax, outPath, header );
+        myWriteToCSV(monthMap, dayMin, dayMax, outPath, header );
+
+ */
         }
-
-
-
-    private static void myWriteToCSV(HashMap<String, Double> map, String outPath, String[] header) {
-// removed null from warning
+        private static void myWriteToCSV(HashMap<String, Double> monthMap, HashMap<String, Double> dayMin, HashMap<String, Double> map, String outPath, String[] header) {
         CSVWriter csv;
         try {
             FileWriter writer = new FileWriter(outPath);
@@ -97,7 +99,7 @@ public class RainfallAnalyser {
                 String key = entry.getKey();
                 Double v = entry.getValue();
                 String[] row = {key, v.toString() };
-                System.out.println(Arrays.toString(row));
+//                System.out.println(Arrays.toString(row));
                 csv.writeNext(row);
             }
             csv.close();
@@ -106,14 +108,13 @@ public class RainfallAnalyser {
         }
 
     }
-
     private static void collectMonth(HashMap<String, Double> map, String[] row) {
         String str = row[RAIN_IDX];
         String key = row[MONTH_IDX];
         double rain = 0;
         try {
             rain = Double.parseDouble(str);
-        } catch (Exception e) {}
+        } catch (Exception ignored) {}
 
         if (map.containsKey(key)){
             double oldRain = map.get(key);
@@ -124,8 +125,6 @@ public class RainfallAnalyser {
             map.put(key, rain);
         }
     }
-
-
     private static void collectDay(HashMap<String, Double> map, String[] row, boolean isMin) {
         String str = row[RAIN_IDX];
         String month = row[MONTH_IDX];
@@ -134,10 +133,9 @@ public class RainfallAnalyser {
         double rain = 0;
         try {
             rain = Double.parseDouble(str);
-        } catch (Exception e) {
+        } catch (Exception ignored) {
 
         }
-
         if (map.containsKey(key)){
             double oldRain = map.get(key);
             double newRain = 0;
@@ -155,4 +153,7 @@ public class RainfallAnalyser {
         }
 
     }
+
+
+
 }
